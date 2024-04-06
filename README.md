00b5ff313348156f4322193fbf7b81520d5af96a
Prajwal1008 <prajwalbankar117@gmail.com>
2024-04-06 22:46:50 +0530

    Create Pages And Routes

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+<!-- 
+1 npm vite + tailwing configuration
+2 Src-pages:about,home,Profile,signin,Signout,Home
+3 npm i reacter-router-dom
+4 App.jsx - import {BrowserRouter, Routes, Route}from 'react-router-dom';
+5 <BrowserRouter>
+   <Routes>
+    <Route path ="/"element={<Home />}></Route>
+   </Routes>
+   </BrowserRouter> -->
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 export default function App() {
   return (
-    <div className='text-red-500'>
-      Hello Prajwal ... ! Glad To See You Here... 
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
+ 
