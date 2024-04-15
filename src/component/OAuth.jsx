47c7d6018b2b425df2c9ea9753b0b3d388ace116
Prajwal1008 <prajwalbankar117@gmail.com>
2024-04-15 10:37:36 +0530

    Google Outh

---
+++ src/component/OAuth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OAuth(){
+    const handleGoogleClick = async()
+
+    return (
+    <button onClick={handleGoogleClick} type ='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with google</button>    
+    )
+}
